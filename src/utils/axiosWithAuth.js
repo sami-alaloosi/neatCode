83208ce4410205bcbb225e198420fdfe05745cad
sami alaloosi <57163390+sami-alaloosi@users.noreply.github.com>
2021-04-26 13:43:07 -0700

    Update axiosWithAuth.js
    Changing  the backend base url

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -6,7 +6,7 @@ export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
   const username = localStorage.getItem("username");
   return axios.create({
-    baseURL: "https://neatcode-backend.herokuapp.com/",
+    baseURL: "https://neatcode2.herokuapp.com/",
     headers: {
       Authorization: token,
       username,
